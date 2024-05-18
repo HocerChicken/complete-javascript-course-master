aefcb3714880a85b85030a29b30e3de26cb2a71b
Pham Thai Hoc <89366106+HocerChicken@users.noreply.github.com>
2024-05-18 23:22:01 +0700

    complete challenge #1

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -29,8 +29,29 @@ const restaurant = {
   order: function (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
+
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = '20:00',
+    address,
+  }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(`Here is your delicious pasta with ${ing1}, ${ing2}, ${ing3}`);
+  },
+
+  orderPizza: function (...Ingredients) {
+    // const ingredients = [...Ingredients];
+    console.log(...Ingredients);
+  },
 };
 /*
+/////////////////////////////////////////////////////
 // Destructuring Array
 const arr = [1, 2, 3];
 
@@ -68,6 +89,7 @@ console.log(r, q, p); // 8 1 1
 
 /////////////////////////////////////////////////////
 // Destructuring Objects
+/*
 const { name, openingHours, categories } = restaurant;
 console.log(name, openingHours, categories);
 
@@ -79,9 +101,281 @@ console.log(menu, starterMenu);
 let a = 111;
 let b = 0;
 
-const obj = {a: 23, b: 7, c: 14};
-({a, b} = obj);
+const obj = { a: 23, b: 7, c: 14 };
+({ a, b } = obj);
 console.log(a, b);
 
 //nested Objects
-const {sat} = openingHours;
+const { sat } = openingHours;
+console.log(sat);
+const {
+  sat: { open: o, close: c },
+} = openingHours;
+console.log(o, c);
+
+restaurant.orderDelivery({ address: 'Ho Chi Minh City' });
+*/
+
+/////////////////////////////////////////////////////
+/*
+// the spread operator
+// spread operator is used to build new arrays or to pass multiple values into a function or method that accepts multiple parameters
+const arr = [7, 8, 9];
+const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+
+const newArr = [1, 2, ...arr];
+console.log(badNewArr);
+console.log(newArr);
+console.log(...newArr);
+
+const newMenu = [...restaurant.mainMenu, 'Gnocci'];
+
+console.log(newMenu);
+console.log(restaurant.mainMenu);
+
+// Copy array
+const mainMenuCopy = [...restaurant.mainMenu];
+console.log('🚀 ~ mainMenuCopy:', mainMenuCopy);
+
+// join 2 arrays
+const menuJoin = [...restaurant.starterMenu, ...restaurant.mainMenu];
+console.log('🚀 ~ menuJoin:', menuJoin);
+
+// Iterables: arrays, strings, maps, sets, NOT objects
+const str = 'Hoang';
+const letters = [...str, ' ', 'Hy'];
+
+const ingredients = [
+  prompt('Ingredient 1: '),
+  prompt('Ingredient 2: '),
+  prompt('Ingredient 3: '),
+];
+
+restaurant.orderPasta(...ingredients); // Here is your delicious pasta with Ingredients <1>, <2>, <3>
+// the spread operator in objects
+const newRestaurant = { foundedIn: 1998, ...restaurant, founder: 'Hoang' };
+*/
+
+////////////////////////////////////////////
+// Rest pattern and parameters
+// Rest pattern and parameters are almost the same as the spread operator,
+// but the rest pattern is used to collect multiple elements and pack them into an array or object.
+// rest parameters are used to pack elements into an array or object.
+/*
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+console.log(a, b, others); // 1 2 [3, 4, 5]
+
+// get the pizza risoto
+
+const [pizza, , risoto, ...otherFoods] = [
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
+];
+console.log(pizza, risoto, otherFoods);
+
+// Objects
+const { sat, ...weeksdays } = restaurant.openingHours;
+console.log(weeksdays);
+
+// Functions
+const add = function (...numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  console.log(sum);
+};
+
+add(2, 3);
+
+const x = [23, 5, 7];
+add(...x);
+restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach');
+*/
+
+///////////////////////////////////////////
+// Short-circuiting (&& and ||)
+// Use any data type, return any data type, short-circuiting
+// && operator returns the first falsy value or the last value if all of them are truthy
+// || operator returns the first truthy value or the last value if all of them are falsy
+/*
+console.log('---OR---');
+console.log(3 || 'Hoang'); // 3
+console.log('' || 'Hoang'); // Hoang
+console.log(true || 0); // true
+console.log(undefined || null); // null
+
+console.log(undefined || null || 0 || null || 'Hoang'); // Hoang
+
+restaurant.numbersOfGirls = parseInt(1);
+
+const beGirlfriend = restaurant.numbersOfGirls || NaN || 'Hoang';
+const guest = restaurant.numbersOfGirls ? restaurant.numbersOfGirls : NaN;
+console.log('log ~ guest:', guest);
+console.log('log ~ beGirlfriend:', beGirlfriend);
+
+console.log('---AND---');
+console.log(3 && 'Hoang'); // 'Hoang'
+console.log('' && 'Hoang'); // ''
+
+//Practical example
+if (restaurant.orderPizza) {
+  restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach');
+}
+
+restaurant.orderPizza && restaurant.orderPizza('cheese', 'tomato');
+*/
+
+/////////////////////////////////////////////
+// Nullish: null and undefined (NOT 0 or '')
+// Nullish Coalescing Operator (??) -> only nullish values (null and undefined)
+/*
+
+console.log('log ~ b:', b);
+*/
+// restaurant.numGuests = 0;
+/*
+const guestCorrect = restaurant.numGuests ?? '10';
+console.log('log ~ guestCorrect:', guestCorrect);
+
+const rest1 = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  numGuest: 0,
+};
+
+const rest2 = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+};
+rest1.numGuest ||= 'Hoc';
+rest1.numGuest ??= 10;
+
+console.log('--rest1--', rest1.numGuest);
+console.log('--rest2--', rest2.numGuest);
+*/
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+We're building a football betting app (soccer for my American friends 😅)!
+
+Suppose we get data from a web service about a certain game (below). In this challenge we're gonna work with the data. So here are your tasks:
+
+1. Create one player array for each team (variables 'players1' and 'players2')
+2. The first player in any player array is the goalkeeper and the others are field players. 
+For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') 
+with all the remaining 10 field players
+3. Create an array 'allPlayers' containing all players of both teams (22 players)
+4. During the game, Bayern Munich (team 1) used 3 substitute players. 
+So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) 
+and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
+7. The team with the lower odd is more likely to win. 
+Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
+
+TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
+
+GOOD LUCK 😀*/
+//1
+const players1 = [
+  'Neuer',
+  'Pavard',
+  'Martinez',
+  'Alaba',
+  'Davies',
+  'Kimmich',
+  'Goretzka',
+  'Coman',
+  'Muller',
+  'Gnarby',
+  'Lewandowski',
+];
+const players2 = [
+  'Ter Stegen',
+  'Pique',
+  'Lenglet',
+  'Alba',
+  'Sergio',
+  'Busquets',
+  'De Jong',
+  'Coutinho',
+  'Suarez',
+  'Messi',
+  'Dembele',
+];
+
+// 2. The first player in any player array is the goalkeeper and the others are field players.
+const [gk, ...fieldPlayers] = players1;
+console.log(gk, fieldPlayers);
+// For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers')
+// with all the remaining 10 field players
+// 3. Create an array 'allPlayers' containing all players of both teams (22 players)
+const allPlayers = [...players1, ...players2];
+console.log('log ~ allPlayers:', allPlayers);
+// 4. During the game, Bayern Munich (team 1) used 3 substitute players.
+// So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'perisic'];
+console.log('log ~ players1Final:', players1Final);
+// 5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+const {
+  odds: { team1: team1, x: draw, team2: team2 },
+} = game;
+console.log(team1, draw, team2);
+// 6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array)
+// and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
+const printGoals = function (...players) {
+  console.log(players);
+  console.log(`the numbers of ${players.length} goals has been scored!!`);
+};
+
+printGoals('a', 'b', 'c');
+printGoals('a', 'b');
+printGoals(...game.scored);
+// 7. The team with the lower odd is more likely to win.
+// Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
+team1 < team2 && console.log('team 1 win');
+team1 > team2 && console.log('team 2 win');
+team1 === team2 && console.log('two team draw');
